38ee39401a8a4e8ad567660a7422caf39678d946
Petter Holmström <petter@vaadin.com>
2016-03-31 15:52:11 +0300

    more WIP

---
+++ src/main/java/org/vaadin/peholmst/samples/dddwebinar/DddWebinarApplication.java
@@ -0,0 +1,12 @@
+package org.vaadin.peholmst.samples.dddwebinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddWebinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddWebinarApplication.class, args);
+	}
+}
